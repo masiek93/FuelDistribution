db86002f1430bee6f7b8b5f3b3fa2c0b670c313d
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2015-10-30 00:00:40 +0100

    initial commit

---
+++ src/main/java/wroc/pwr/FuelDistribution/App.java
@@ -0,0 +1,13 @@
+package wroc.pwr.FuelDistribution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
